a7cefeaf65289038e39f5bfc590b5146f1b962ba
Mike Branski <mikebranski@gmail.com>
2011-08-10 15:07:03 -0500

    Remove GPL license file and link to gnu.org instead

--- jquery.nospam.js
+++ jquery.nospam.js
@@ -4,14 +4,14 @@
  * mikebranski@gmail.com
  *
  * Copyright (c) 2008-2011 Mike Branski (https://github.com/mikebranski)
- * Licensed under GPL (https://github.com/mikebranski/jquery-nospam/blob/master/gpl.txt)
+ * Licensed under GPL version 3 (http://www.gnu.org/copyleft/gpl.html)
  *
  * NOTE: This script requires jQuery to work.  Download jQuery at jquery.com
  *
  * Thanks to Bill on the jQuery mailing list for the double slash idea!
  *
  * CHANGELOG:
- * v 1.3.1 - Updated links in comments, tested against jQuery 1.6.2
+ * v 1.3.1 - Updated links in comments, removed GPL license file, tested against jQuery 1.6.2
  * v 1.3   - Added support for e-mail addresses with multiple dots (.) both before and after the at (@) sign
  * v 1.2.1 - Included GPL license
  * v 1.2   - Finalized name as No Spam (was Protect Email)
